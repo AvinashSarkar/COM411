f3f4924a3cc37a0b8eb306f74e68e3c54217df5f
AvinashSarkar <2sarka38@solent.ac.uk>
2024-09-30 14:50:45 +0100

    Added code to demonstrate the use of comparison operators.

--- week2/Operators_if_statements.py
+++ week2/Operators_if_statements.py
@@ -5,3 +5,15 @@ if num % 2 == 0:
     print(f"The number {num} is even")
 else:
     print(f"The number {num} is odd")
+
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+
+if num1 > num2:
+    print("The second number is the smallest")
+elif num2 > num1:
+    print("The first number is the smallest")
+elif num1 == num2:
+    print("Both numbers are equal")
