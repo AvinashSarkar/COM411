7212b908c4d658253d08ab3cf24c57f1eba3e4a3
AvinashSarkar <2sarka38@solent.ac.uk>
2024-09-30 14:06:14 +0100

    if statements

---
+++ week2/if_statements.py
@@ -0,0 +1,9 @@
+print("Where are you?")
+location = input()
+
+if location == "southampton":
+    print("set alarm to 8:00")
+elif location == "eastleigh":
+    print("set alarm to 7:00")
+else:
+    print("set alarm to 6:00")
