bc4fcdce16fe0ced3a1d74640ef28093a051d4a7
AvinashSarkar <2sarka38@solent.ac.uk>
2024-10-14 14:48:47 +0100

    Added code to demonstrate the use of built-in functions: abs, range, chr.

--- week4/built_in_functions.py
+++ week4/built_in_functions.py
@@ -15,3 +15,27 @@ print("Program Ended.")
 
 if __name__ == "__main__":
     main()
+
+
+def main():
+    print("Program Started!")
+
+    while True:
+        try:
+            user = int(input("Enter an ASCII code (0-255): "))
+
+            if 0 <= user <= 255:
+                character = chr(user)
+                print(f"The character represented by the ASCII code {user} is '{character}' ")
+                break
+            else:
+
+                print("Invalid input. Please enter a number between 0 and 255.")
+        except ValueError:
+
+            print("Invalid input. Please enter a valid integer.")
+
+    print("Program Ended.")
+
+if __name__ == "__main__":
+    main()
